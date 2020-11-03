18565e7dbbfd018d8221da141bb687cdb19b32f2
guxal <jonathanacp93@gmail.com>
2020-11-03 11:27:17 -0500

    fix with gofmt

--- application_structure/main.go
+++ application_structure/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 const secondsInHours = 3600
 
-func main(){
+func main() {
 	fmt.Println("Hello Go World!!")
 	distance := 60.8
 
-	fmt.Printf("The distance in miles is %f \n", distance * 0.62137)
+	fmt.Printf("The distance in miles is %f \n", distance*0.62137)
 }
