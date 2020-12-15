81d96218655da7ee47258902764a53d831493171
guxal <jonathanacp93@gmail.com>
2020-12-15 09:09:49 -0500

    add if, else if and else

---
+++ flow_control/if1/README.md
@@ -0,0 +1,1 @@
+# if, Else if and Else
---
+++ flow_control/if1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	price, inStock := 100, true
+
+	if price > 80 {
+		fmt.Println("Too Expensive!")
+	}
+
+	// _ = inStock
+
+	if price <= 100 && inStock {
+		fmt.Println("Buy it!")
+	}
+
+	// if price {
+	// 	fmt.Println("something")
+	// }
+
+	if price < 100 {
+		fmt.Println("It's cheap!")
+	} else if price == 100 {
+		fmt.Println("On the edge")
+	} else {
+		fmt.Println("It's Expensive!")
+	}
+
+	age := 50
+
+	if age >= 0 && age < 18 {
+		fmt.Println("You cannot vote! Please return in %d years !\n", 18-age)
+	} else if age == 18 {
+		fmt.Printf("This is your first vote!")
+	} else if age > 18 && age <= 100 {
+		fmt.Printf("Please vote, it's important!!")
+	} else {
+		fmt.Printf("Invalid age!")
+	}
+}
