4312ed35d2ee64963514221a173f989b65a1f5b6
guxal <jonathanacp93@gmail.com>
2021-01-23 17:34:46 -0500

    add functions basics

---
+++ functions/functions_basics/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// defining a function with no parameters
+func f1() {
+	fmt.Println("This is f1() function.")
+}
+
+// defining a function with 2 parameters, a and b
+func f2(a int, b int) {
+    //a and b are local to the function
+	fmt.Println("Sum:", a + b)
+}
+
+// defining a function using shorthand parameters notation
+func f3(a, b, c int, d, e float64, s string) {
+	fmt.Println(a, b, c, d, e, s)
+}
+
+// defining a function that have one parameter of type float64 and returns a value of type float64
+func f4(a float64) float64 {
+	return math.Pow(a, a)
+    //any statements below the return statement are never executed
+}
+
+// defining a function that have two parameters of type int and returns two values of type int
+func f5 (a, b int) (int, int) {
+	return a+b, a*b
+}
+
+// defining a function that have one parameter of type int and returns a "named parameter"
+func sum(a, b int)(s int) {
+	fmt.Println(s) // -> s is a variable with the zero value inside the function
+	s = a + b
+	// it automatically return s
+	return  // This is known as a "naked" return.
+}
+
+func main() {
+	// calling functions
+	f1() // => This is f1() function
+	f2(5, 7)
+	f3(4, 5, 6, 4.4, 7.8, "golang")
+	p := f4(5.1)
+	fmt.Println(p)
+
+	a, b := f5(8, 9)
+	fmt.Println(a, b) // => a:17, b:72
+
+	mySum := sum(4, 8)
+	fmt.Println(mySum) // -> 12
+}
