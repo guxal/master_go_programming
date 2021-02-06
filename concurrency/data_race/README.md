1b4bfdb3e6d719a7056069bae50fcfa5aa8cb752
guxal <jonathanacp93@gmail.com>
2021-02-06 13:15:15 -0500

    add data race

---
+++ concurrency/data_race/README.md
@@ -0,0 +1,8 @@
+# data race
+
+
+detecte race condition when compiling with this flag
+
+```bash
+go run -race ./main.go
+```
---
+++ concurrency/data_race/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"time"
+	"fmt"
+)
+
+func main() {
+	const gr = 100
+
+	var wg sync.WaitGroup
+
+	wg.Add(gr * 2)
+
+	var n int = 0
+
+	for i := 0; i < gr; i++ {
+		go func() {
+			time.Sleep(time.Second / 10)
+			n++
+			wg.Done()
+		}()
+
+		go func() {
+			time.Sleep(time.Second / 10)
+			n--
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	fmt.Println("Final value of n:", n)
+}
