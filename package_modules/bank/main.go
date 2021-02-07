34385829a33efcb3e31a407caab8608b1cd7a792
guxal <jonathanacp93@gmail.com>
2021-02-07 15:58:40 -0500

    add functios and math package

--- package_modules/bank/main.go
+++ package_modules/bank/main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
-	var x uint = 40
+	var x int = 40
 	fmt.Printf("%d is even: %t \n", x,numbers.Even(x))
+	fmt.Println(numbers.IsPrime(13), numbers.IsPrime(100))
+	fmt.Println(numbers.OddAndPrime(25))
+	fmt.Println(numbers.OddAndPrime(13))
 }
--- package_modules/my_packages/numbers/numbers.go
+++ package_modules/my_packages/numbers/numbers.go
@@ -1,8 +1,31 @@
 package numbers
 
-func Even(n uint) bool {
+func Even(n int) bool {
 	if n % 2 == 0 {
 		return true
 	}
 	return false
+}
+
+func odd(n int) bool {
+	if n%2 != 0 {
+		return true
+	}
+	return false
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func OddAndPrime(n int) bool {
+	if odd(n) && isPrime(n){
+		return false
+	}
+	return true
 }
